02acbef8d6704456f38600e84822acb56194d7aa
Rumen Orachev <rumen.orachev@outlook.es>
2025-04-10 20:13:09 +0200

    actualizacion movil

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,11 +1,11 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
+});
 
 function toggleMenu() {
   const navLinks = document.querySelector('.nav-links');
